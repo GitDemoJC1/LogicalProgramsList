bbad6abd73e38451892491b7503c68d58cf31406
Dev-1 <ashok.ja008@gmail.com>
2022-06-13 07:43:07 +0530

    one sysout and syserr statments are added

--- src/com/jc/main/Cilent.java
+++ src/com/jc/main/Cilent.java
@@ -5,6 +5,8 @@ import com.jc.stringprograms.ReverseString;
 public class Cilent {
 	public static void main(String[] args) {
 		System.out.println(ReverseString.reverseWord("Swathi"));
+		System.out.println();
+		System.err.println(ReverseString.reverseWord("JAVA DEVELOPER"));
 	}
 
 }
