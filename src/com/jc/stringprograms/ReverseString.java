69e6a9c4f7a445faa02f83af1a59e289e2b99a4c
Dev-1 <ashok.ja008@gmail.com>
2022-06-13 07:32:35 +0530

    Tk987:Added a reverse string  logic

--- src/com/jc/stringprograms/ReverseString.java
+++ src/com/jc/stringprograms/ReverseString.java
@@ -1,5 +1,12 @@
 package com.jc.stringprograms;
 
 public class ReverseString {
+	public static String reverseWord(String s) {
+		String str = "";
+		for (int i = s.length() - 1; i >= 0; i--) {
+			str += s.charAt(i);
+		}
+		return str;
+	}
 
 }
