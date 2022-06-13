833d13ad2a29cdacf44fb3992374d9f879dc09e9
Dev-1 <ashok.ja008@gmail.com>
2022-06-13 07:35:38 +0530

    Merge branch 'master' of https://github.com/GitDemoJC1/LogicalProgramsList.git

--- src/com/jc/stringprograms/ReverseString.java
+++ src/com/jc/stringprograms/ReverseString.java
@@ -9,11 +9,4 @@ public class ReverseString {
 		return str;
 	}
 
-	public static String reverseWord(String s) {
-		String str = "";
-		for (int i = s.length() - 1; i >= 0; i--) {
-			str += s.charAt(i);
-		}
-		return str;
-	}
 }
