91f640d394071ad448fdcbd613cdc2ee9a20a95d
TL <ashok.ja008@gmail.com>
2022-06-13 06:22:47 +0530

    two new java classes are created

---
+++ src/com/jc/main/Cilent.java
@@ -0,0 +1,8 @@
+package com.jc.main;
+
+public class Cilent {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/jc/stringprograms/ReverseString.java
@@ -0,0 +1,5 @@
+package com.jc.stringprograms;
+
+public class ReverseString {
+
+}
